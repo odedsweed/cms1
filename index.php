e1d81fdbae6ccd08781bcbd26fdd5c72e4aa6656
odedsweed <oded1982@hotmail.com>
2013-02-12 10:49:41 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+session_start();
+?>
+
+<!DOCTYPE HTML>
+
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+
+<head>
+  	<title>cms1</title>
+		<link rel="stylesheet" type="text/css" href="/style/default.css">
+</head>
+
+<body>
+	
+
+	
+	
+	
+	
+	
+	
+</body>
+</html>
